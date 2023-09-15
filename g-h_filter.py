ad329fea842c87caabc7a02e055043c3b51a69b4
brucew5978-git <83440706+brucew5978-git@users.noreply.github.com>
2023-09-16 00:38:42 +0800

    Add files via upload
    Updated graphs

--- g-h_filter.py
+++ g-h_filter.py
@@ -20,17 +20,30 @@ def g_h_filter(data, x0, dx, g, h, dt=1.):
 weights = np.array([158.0, 164.2, 160.3, 159.9, 162.1, 164.6, 169.6, 167.4, 166.4, 171.0, 171.2, 172.6])
 
 # Actual weight plot
-plt.plot(range(len(weights)), weights, label='Measured weight', marker='o')
+plt.plot(range(len(weights)), weights, label='Measured State', marker='o')
 
 # Apply the g-h filter to the data
 filtered_data = g_h_filter(data=weights, x0=160., dx=1., g=6./10, h=2./3, dt=1.)
 
 # Filtered weight plot
-plt.plot(range(len(filtered_data)), filtered_data, label='Filtered weight', linestyle='--', marker='x')
+plt.plot(range(len(filtered_data)), filtered_data, label='Filtered State', linestyle='--', marker='x')
+
+start_value, end_value = 160, 172
+num_increments = 12
+
+# Generate evenly spaced x-values
+x_values = np.arange(num_increments)
+
+# Corresponding y-values (you can modify this as needed)
+y_values = np.linspace(start_value, end_value, num=num_increments)
+
+# Create the line plot
+plt.plot(x_values, y_values, label='Actual State', linestyle='-')
 
 # Add labels and legend
 plt.xlabel('Time Step')
-plt.ylabel('Weight')
+plt.ylabel('State')
+plt.title('Figure 1: Comparison of Measured and Filter state against actual state')
 plt.legend()
 
 # Show the plot
